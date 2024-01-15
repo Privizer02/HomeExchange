b20559809802a87c2065031ff5309341f96f1fe3
Privizer02 <milanprivizer2002@gmail.com>
2024-01-15 16:57:52 +0100

    html/nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './core/home/home.component';
+import { RentComponent } from './rent/rent.component';
+import { BuyComponent } from './core/buy/buy.component';
+import { SellComponent } from './core/sell/sell.component';
+import { ContactComponent } from './core/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'rent',component:RentComponent},
+  {path:'buy',component:BuyComponent},
+  {path:'sell',component:SellComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'',redirectTo:'/home',pathMatch:'prefix'},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from './core/nav/nav.component';
+import { ContactComponent } from './core/contact/contact.component';
+import { BuyComponent } from './core/buy/buy.component';
+import { SellComponent } from './core/sell/sell.component';
+import { RentComponent } from './rent/rent.component';
+import { HomeComponent } from './core/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ContactComponent,
+    BuyComponent,
+    SellComponent,
+    RentComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
