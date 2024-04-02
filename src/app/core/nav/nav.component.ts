86997c6cb530c42f7be4a2e32c79f4f3cde5263b
Privizer02 <milanprivizer2002@gmail.com>
2024-04-02 21:23:58 +0200

    navbar fix

--- src/app/core/nav/nav.component.ts
+++ src/app/core/nav/nav.component.ts
@@ -4,26 +4,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent {
+  mobileMedia: any = window.matchMedia('(max-width:800px');
 
-  mobileMedia:any=window.matchMedia("(max-width:800px")
-
-	constructor() {
-    if(this.mobileMedia.matches) {
-      this.menu = !this.menu;
-      console.log("work")
+  constructor() {
+    if (this.mobileMedia.matches) {
+      this.menu = false;
     }
-	}
-  
-
-  menu:boolean=true;
-  openMenu() {
-    
-    this.menu = !this.menu;
   }
 
-  
-
+  menu: boolean = true;
+  openMenu() {
+    if (this.mobileMedia.matches) {
+      this.menu = !this.menu;
+    } else {
+      return;
+    }
+  }
 }
